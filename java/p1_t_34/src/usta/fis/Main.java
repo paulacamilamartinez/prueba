6935039dfcd52c71597d2756491af3eaa7bc729e
paula camila martinez <paula.martinezp@ustatunja.edu.co>
2021-03-26 08:14:18 -0500

    java 1

---
+++ java/p1_t_34/src/usta/fis/Main.java
@@ -0,0 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package usta.fis;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(string[] args) {
+        System.out.println("-------------------------------");
+        System.out.println("Softhelloworld V1.0");
+        System.out.println("MAKER:Paula Camila Martinez");
+        System.out.println("-------------------------------");
+        System.out.println("Hello world");
+    }
+}
